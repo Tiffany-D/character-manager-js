883c1bf32245d8fd913018d01dd73f039647a26d
Tiffany Dessouroux <tiffany.dessouroux@gmail.com>
2021-10-25 17:01:25 +0200

    trying to make js work

--- js/script.js
+++ js/script.js
@@ -1,2 +1,30 @@
+import "regenerator-runtime/runtime";
+import "babel-polyfill";
 
+const axios = require('axios');
+
+
+document.querySelector('#see-character').addEventListener("click", async () => {
     
+    const api = await axios.get('https://character-database.becode.xyz/characters');
+    const arrayCharacters = api.data;
+    console.log(arrayCharacters);
+    arrayCharacters.forEach(element=> {
+        console.log(element);
+    });
+    
+})
+
+function displayName() {
+    const nameOfTheCharacter = document.querySelector('h5');
+    
+    
+}
+
+function displayDescription() {
+    const shortDescription = document.querySelector('p');
+}
+
+function displayPicture() {
+    const image = document.querySelector('img');
+}
